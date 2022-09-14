d76b77efc8a98c71640b167fd3ee49f0a9ddb145
Juan Manuel Ponce <poncejuanmanuel451@gmail.com>
2022-09-14 20:22:56 -0300

    Primera pre entrega del proyecto final

--- Proyecto/JS/miarchivo.js
+++ Proyecto/JS/miarchivo.js
@@ -1,21 +1,35 @@
+let productos = []
+let carrito = []
+
 function menu(){ 
-    let opciones = prompt(`Bienvenido a Blanco and Co! Ingrese la opción que desea ver:
-1. Colchones
-2. Sommiers
-3. Salir`);
-    switch(opciones){
-        case "1":
-            Colchones()
-        break;
-        case "2":
-            Sommiers()
-        break;
-        case "3":
-            alert("Lo esperamos nuevamente!")
+    let opciones = "1";
+    while (opciones !== "3") {
+        opciones = prompt(`Bienvenido a Blanco and Co! Ingrese la opción que desea ver:
+        1. Colchones
+        2. Sommiers
+        3. Mostrar carrito
+        4. Salir`);
+        switch(opciones){
+            case "1":
+                Colchones()
+                break;
+            case "2":
+                Sommiers()
+                break;
+            case "3":
+                alert(`Su carrito contiene los siguientes items: ${carrito}
+                y en total suman $ ${valorCarrito}`);
+                break;
+            case "4":
+                alert("Lo esperamos nuevamente!")
+                break;
+            default:
+                alert("Opcion incorrecta");
+        }
     }
 }
 
-let carrito = 0
+let valorCarrito = 0;
 
 function Colchones(){
     let opciones = prompt(`1. Espuma Alta Densidad
@@ -25,12 +39,14 @@ function Colchones(){
             let espuma = prompt(`1. Línea Dorada $30000
     2. Nimbus $40000`)
             if (espuma == "1"){
-                carrito = 30000
-                alert(`Su carrito tiene un valor de $ ${carrito}`)
+                carrito.push(lineaDorada.nombre);
+                valorCarrito += 30000
+                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
             }
             else if (espuma == "2"){
-                carrito = 40000
-                alert(`Su carrito tiene un valor de $ ${carrito}`)
+                carrito.push(nimbus.nombre);
+                valorCarrito += 40000
+                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
             }
             else{
                 alert("Debe ingresar una opción posible")
@@ -40,12 +56,14 @@ function Colchones(){
             let resortes = prompt(`1. Finesse $60000
     2. Brighton $90000`)
             if (resortes == "1"){
-                carrito = 60000
-                alert(`Su carrito tiene un valor de $ ${carrito}`)
+                carrito.push(finesse.nombre);
+                valorCarrito += 60000
+                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
             }
             else if (resortes == "2"){
-                carrito = 90000
-                alert(`Su carrito tiene un valor de $ ${carrito}`)
+                carrito.push(brighton.nombre);
+                valorCarrito += 90000
+                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
             }
             else{
                 alert("Debe ingresar una opción posible")
@@ -65,12 +83,14 @@ function Sommiers(){
             let kingkoil = prompt(`1. Confort Sensations $15000
     2. World Luxury Tejido de Punto $20000`)
         if (kingkoil == "1"){
-            carrito = 15000
-            alert(`Su carrito tiene un valor de $ ${carrito}`)
+            carrito.push(confortSens.nombre);
+            valorCarrito += 15000
+            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
         }
         else if (kingkoil == "2"){
-            carrito = 20000
-            alert(`Su carrito tiene un valor de $ ${carrito}`)
+            carrito.push(tejidoPunto.nombre);
+            valorCarrito += 20000
+            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
         }
         else{
             alert("Debe ingresar una opción posible")
@@ -80,12 +100,14 @@ function Sommiers(){
             let inducol = prompt(`1. Imperial $10000
     2. Inducol $12000`)
         if (inducol == "1"){
-            carrito = 10000
-            alert(`Su carrito tiene un valor de $ ${carrito}`)
+            carrito.push(imperial.nombre);
+            valorCarrito += 10000
+            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
         }
         else if (inducol == "2"){
-            carrito = 12000
-            alert(`Su carrito tiene un valor de $ ${carrito}`)
+            carrito.push(inducol.nombre);
+            valorCarrito += 12000
+            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
         }
         else{
             alert("Debe ingresar una opción posible")
@@ -100,3 +122,20 @@ function Sommiers(){
 menu();
 
 
+class Producto{
+    constructor(id,nombre,precio,tipo){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.tipo = tipo;
+    }
+}
+
+const lineaDorada = new Producto(1, "Linea Dorada", 30000, 1);
+const nimbus = new Producto(2, "Nimbus", 40000, 1);
+const finesse = new Producto(3, "Finesse", 60000, 1);
+const brighton = new Producto(4, "Brighton", 90000, 1);
+const confortSens = new Producto(5, "Confort Sensations", 15000, 2);
+const tejidoPunto = new Producto(6, "World Luxury Tejido de Punto", 20000, 2);
+const imperial = new Producto(7, "Imperial", 10000, 2);
+const inducol = new Producto(8, "Inducol", 12000, 2);
