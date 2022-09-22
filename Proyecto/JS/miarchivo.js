20896fcdc51bfb18ea5c3057caf1e349cffbac1d
Juan Manuel Ponce <poncejuanmanuel451@gmail.com>
2022-09-21 23:35:26 -0300

    Pre entrega numero 2 del proyecto final

--- Proyecto/JS/miarchivo.js
+++ Proyecto/JS/miarchivo.js
@@ -1,127 +1,3 @@
-let productos = []
-let carrito = []
-
-function menu(){ 
-    let opciones = "1";
-    while (opciones !== "3") {
-        opciones = prompt(`Bienvenido a Blanco and Co! Ingrese la opción que desea ver:
-        1. Colchones
-        2. Sommiers
-        3. Mostrar carrito
-        4. Salir`);
-        switch(opciones){
-            case "1":
-                Colchones()
-                break;
-            case "2":
-                Sommiers()
-                break;
-            case "3":
-                alert(`Su carrito contiene los siguientes items: ${carrito}
-                y en total suman $ ${valorCarrito}`);
-                break;
-            case "4":
-                alert("Lo esperamos nuevamente!")
-                break;
-            default:
-                alert("Opcion incorrecta");
-        }
-    }
-}
-
-let valorCarrito = 0;
-
-function Colchones(){
-    let opciones = prompt(`1. Espuma Alta Densidad
-2. Resortes`)
-    switch (opciones){
-        case "1":
-            let espuma = prompt(`1. Línea Dorada $30000
-    2. Nimbus $40000`)
-            if (espuma == "1"){
-                carrito.push(lineaDorada.nombre);
-                valorCarrito += 30000
-                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-            }
-            else if (espuma == "2"){
-                carrito.push(nimbus.nombre);
-                valorCarrito += 40000
-                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-            }
-            else{
-                alert("Debe ingresar una opción posible")
-            }
-        break;
-        case "2":
-            let resortes = prompt(`1. Finesse $60000
-    2. Brighton $90000`)
-            if (resortes == "1"){
-                carrito.push(finesse.nombre);
-                valorCarrito += 60000
-                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-            }
-            else if (resortes == "2"){
-                carrito.push(brighton.nombre);
-                valorCarrito += 90000
-                alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-            }
-            else{
-                alert("Debe ingresar una opción posible")
-            }
-        break;
-        default:
-            alert("Ingrese una opcion correcta")
-        break;
-    }
-}
-
-function Sommiers(){
-    let opciones = prompt(`1. King Koil
-2. Inducol`)
-    switch (opciones){
-        case "1":
-            let kingkoil = prompt(`1. Confort Sensations $15000
-    2. World Luxury Tejido de Punto $20000`)
-        if (kingkoil == "1"){
-            carrito.push(confortSens.nombre);
-            valorCarrito += 15000
-            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-        }
-        else if (kingkoil == "2"){
-            carrito.push(tejidoPunto.nombre);
-            valorCarrito += 20000
-            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-        }
-        else{
-            alert("Debe ingresar una opción posible")
-        }
-            break;
-        case "2":
-            let inducol = prompt(`1. Imperial $10000
-    2. Inducol $12000`)
-        if (inducol == "1"){
-            carrito.push(imperial.nombre);
-            valorCarrito += 10000
-            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-        }
-        else if (inducol == "2"){
-            carrito.push(inducol.nombre);
-            valorCarrito += 12000
-            alert(`Su carrito tiene un valor de $ ${valorCarrito}`)
-        }
-        else{
-            alert("Debe ingresar una opción posible")
-        }
-        break;
-        default:
-            alert("Ingrese una opcion correcta")
-        break;
-    }
-}
- 
-menu();
-
-
 class Producto{
     constructor(id,nombre,precio,tipo){
         this.id = id;
@@ -129,13 +5,56 @@ class Producto{
         this.precio = precio;
         this.tipo = tipo;
     }
+    mostrarProducto() {
+        return this.nombre + " " + "$" + this.precio;
+    }
 }
 
-const lineaDorada = new Producto(1, "Linea Dorada", 30000, 1);
-const nimbus = new Producto(2, "Nimbus", 40000, 1);
-const finesse = new Producto(3, "Finesse", 60000, 1);
-const brighton = new Producto(4, "Brighton", 90000, 1);
-const confortSens = new Producto(5, "Confort Sensations", 15000, 2);
-const tejidoPunto = new Producto(6, "World Luxury Tejido de Punto", 20000, 2);
-const imperial = new Producto(7, "Imperial", 10000, 2);
-const inducol = new Producto(8, "Inducol", 12000, 2);
+const productos = [];
+
+const prod1 = new Producto(1, "Linea Dorada", 30000, 1);
+const prod2 = new Producto(2, "Nimbus", 40000, 1);
+const prod3 = new Producto(3, "Finesse", 60000, 1);
+const prod4 = new Producto(4, "Brighton", 90000, 1);
+const prod5 = new Producto(5, "Confort Sensations", 15000, 2);
+const prod6 = new Producto(6, "World Luxury Tejido de Punto", 20000, 2);
+const prod7 = new Producto(7, "Imperial", 10000, 2);
+const prod8 = new Producto(8, "Inducol", 12000, 2);
+
+productos.push(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8);
+
+function mostrarProductos(productos){
+    const contenedor = document.getElementById("container");
+    console.log(contenedor);
+    productos.forEach(producto => {
+        const card = document.createElement('card');
+        card.innerHTML += 
+                        `<div class="card" style="">
+                            <h5 class="card-title"> ${producto.nombre}</h5>
+                            <p class="card-text"> Precio: ${producto.precio}</p>
+                            <button class="" id="button${producto.id}"> Agregar al carrito </button>
+                        </div>`
+        contenedor.appendChild(card);
+        
+        const button = document.getElementById(`button${producto.id}`);
+        button.addEventListener('click', () =>{
+            productosCarrito(`${producto.id}`)
+            alert(`${producto.nombre} fue agregado al carrito!`)
+        })
+    });
+}
+
+class DetallePedido{
+    constructor(producto, cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        }
+        mostrarDetalle() {
+            return this.cantidad + "x " + this.producto.nombre + " ($" + this.calcularMontoDetalle() + ")";
+        }
+        calcularMontoDetalle(){
+            return this.cantidad * this.producto.precio;
+        }
+}
+
+mostrarProductos(productos);
